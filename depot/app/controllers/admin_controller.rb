cd0b2f3163731de578e5b00c50cbd4c230a675a4
xhanshawn <xhan@wpi.edu>
2015-05-09 18:04:30 -0400

    Logging in added

--- depot/app/controllers/admin_controller.rb
+++ depot/app/controllers/admin_controller.rb
@@ -6,18 +6,8 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class ApplicationController < ActionController::Base
-  before_action :authorize
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-    # ...
-
-  protected
-
-    def authorize
-      unless User.find_by(id: session[:user_id])
-        redirect_to login_url, notice: "Please log in"
-      end
-    end
+class AdminController < ApplicationController
+  def index
+    @total_orders = Order.count
+  end
 end
