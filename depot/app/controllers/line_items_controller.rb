4e370d66ccf8bcc76ec1feac0b060eee3e930faf
xhanshawn <xhan@wpi.edu>
2015-05-09 18:23:33 -0400

    depot:language
    Add es for depot application

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -1,4 +1,13 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class LineItemsController < ApplicationController
+  skip_before_action :authorize, only: :create
   include CurrentCart
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
@@ -43,16 +52,15 @@ class LineItemsController < ApplicationController
     end
   end
 
-
   # PATCH/PUT /line_items/1
   # PATCH/PUT /line_items/1.json
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
@@ -63,7 +71,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to line_items_url }
       format.json { head :no_content }
     end
   end
@@ -74,8 +82,10 @@ class LineItemsController < ApplicationController
       @line_item = LineItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, only allow the white
+    # list through.
     def line_item_params
       params.require(:line_item).permit(:product_id)
     end
+  #...
 end
